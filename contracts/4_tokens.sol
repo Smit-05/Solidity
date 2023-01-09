3563b4a81e3a3ba32edcf98fc0d979674d5a9623
Smit-05 <smitpadaliya5@gmail.com>
2023-01-09 19:22:25 +0530

    token transfer

---
+++ contracts/4_tokens.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.1;
+
+contract Token{
+
+    mapping(address => uint256) public balances;
+    address payable wallet;
+
+    constructor(address payable _wallet) public {
+        wallet = _wallet;
+    }
+
+    function() external payable{
+        buyToken();
+    }
+
+    function buyToken() public payable {
+        balances[msg.sender] += 1;
+        wallet.transfer(msg.value);
+    }
+}
